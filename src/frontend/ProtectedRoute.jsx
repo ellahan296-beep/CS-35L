e921a1a181c5f447cef5307325f51f07c76b3e87
Ella Han <ellahan296@gmail.com>
2026-06-05 14:22:23 -0700

    revert previous commit for protected route, bug

--- src/frontend/ProtectedRoute.jsx
+++ src/frontend/ProtectedRoute.jsx
@@ -1,18 +1,8 @@
 import { Navigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { isLoggedIn } from '../services/profileService.js'
 
 function ProtectedRoute({ children }) {
-  const [loggedIn, setLoggedIn] = useState(false);
-  
-  useEffect(() => {
-    isLoggedIn()
-    .then(data => {
-      setLoggedIn(data);
-    })
-  }, []);
-
-  if (!loggedIn) {
+  const token = localStorage.getItem('token');
+  if (!token) {
     return <Navigate to="/login" replace />;
   }
   return children;
